5e9c03146455c3acf27e1c1b4cfce025017de9c7
kumar@kashish <mobiledevelopers74@gmail.com>
2024-01-10 11:54:32 +0530

    modified server file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const app = express();
-app.use((res,req)=>{
-        res.json({message:"Hiiii-----.."})
-})
+
 const PORT= "9000"
 app.listen(PORT,()=>{
     console.log("app is running on port ",PORT)
